2b249f2183d0806b1f64039c9fccc15aec94b2cd
Tom Stak <stactk49@Toms-MacBook-Pro.local>
2017-01-11 12:48:37 -0500

    Removed unneccessary folder

---
+++ finalProject/finalProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  finalProject
+//
+//  Created by Tom Stak on 2017.01.11.
+//  Copyright © 2017 Jerry & Tom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //Jerry:  This is a test
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
